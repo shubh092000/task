eb0dec93bb6352254fc85765f0786e190e8f68fe
Shubham Paliwal <87996236+shubh092000@users.noreply.github.com>
2025-05-09 17:26:55 +0530

    Update main.tf

--- TF/main.tf
+++ TF/main.tf
@@ -54,7 +54,7 @@ resource "aws_security_group" "eks_cluster_sg" {
 }
 
 
-data "aws_availability_zones" "available" {}
+
 
 # EKS Cluster Role
 resource "aws_iam_role" "eks_cluster_role" {
@@ -63,16 +63,7 @@ resource "aws_iam_role" "eks_cluster_role" {
   assume_role_policy = data.aws_iam_policy_document.eks_assume_role.json
 }
 
-data "aws_iam_policy_document" "eks_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
-}
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster_role.name
@@ -105,16 +96,7 @@ resource "aws_iam_role" "eks_node_group_role" {
   assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role.json
 }
 
-data "aws_iam_policy_document" "eks_node_assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
 
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
-}
 
 # Attach IAM Policies for Node Group
 resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKSWorkerNodePolicy" {
