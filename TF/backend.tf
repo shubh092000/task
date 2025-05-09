05fe152cbc45dbf500b4cf26db45420472558d03
Shubham Paliwal <87996236+shubh092000@users.noreply.github.com>
2025-05-09 18:11:13 +0530

    Update backend.tf

--- TF/backend.tf
+++ TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "awscicdbuckettf"       
-    key            = "state-file2/"
+    key            = "state-file2/terraform.tfstate"  
     region         = "us-east-1"           
     encrypt        = true
   }
